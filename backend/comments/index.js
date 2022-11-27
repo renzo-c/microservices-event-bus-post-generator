bd34d0b8e6d2fbf84a4c0b0208ca3f16c24cc91f
Renzo Navarro <rcnavarrop@gmail.com>
2022-11-27 12:14:42 -0500

    Fix bug with comment apis

--- backend/comments/index.js
+++ backend/comments/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const { v4: uuidv4 } = require("uuid");
 const cors = require("cors");
+const axios = require('axios');
 
 const app = express();
 app.use(bodyParser.json());
@@ -10,18 +11,16 @@ app.use(cors());
 const commentsByPostId = {};
 // console.log({commentsByPostId})
 
-app.get("posts/:id/comments", (req, res) => {
-  res.send(commentsByPostId[req.params.id]);
+app.get("/posts/:id/comments", (req, res) => {
+  res.send(commentsByPostId[req.params.id] || []);
 });
 
-app.post("posts/:id/comments", (req, res) => {
+app.post("/posts/:id/comments", (req, res) => {
+  console.log("this is a post req")
   const commentId = uuidv4();
-  console.log({commentId})
   const { content } = req.body;
-  console.log({content})
   const comments = commentsByPostId[req.params.id] || [];
-  console.log({id: req.params.id})
-
+console.log({comments})
   comments.push({ id: commentId, content });
 
   commentsByPostId[req.params.id] = comments;
--- backend/posts/index.js
+++ backend/posts/index.js
@@ -2,27 +2,33 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const { v4: uuidv4 } = require("uuid");
 const cors = require("cors");
+const axios = require('axios');
 
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
 const posts = {};
-console.log({posts})
+console.log({ posts });
 
 app.get("/posts", (req, res) => {
   res.send(posts);
 });
 
-app.post("/posts", (req, res) => {
+app.post("/posts", async (req, res) => {
   const id = uuidv4();
   const { title } = req.body;
-  
+
   posts[id] = {
     id,
     title,
   };
-  
+
+  await axios.post("http://localhost:4005/events", {
+    type: "PostCreated",
+    data: { id, title },
+  });
+
   res.status(201).send(posts[id]);
 });
 
