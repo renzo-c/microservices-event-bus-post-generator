585b1f8f968a76a7680f81e471a0e6e20ff9d258
Renzo Navarro <rcnavarrop@gmail.com>
2022-11-30 23:05:56 -0500

    Update comment content after moderation in query service

--- backend/comments/index.js
+++ backend/comments/index.js
@@ -19,7 +19,9 @@ app.post("/posts/:id/comments", async (req, res) => {
   const commentId = uuidv4();
   const { content } = req.body;
   const comments = commentsByPostId[req.params.id] || [];
-  comments.push({ id: commentId, content });
+  const status = "pending";
+
+  comments.push({ id: commentId, content, status });
 
   commentsByPostId[req.params.id] = comments;
 
@@ -29,13 +31,34 @@ app.post("/posts/:id/comments", async (req, res) => {
       id: commentId,
       content,
       postId: req.params.id,
+      status,
     },
   });
   res.status(201).send(comments);
 });
 
-app.post("/events", (req, res) => {
+app.post("/events", async (req, res) => {
   console.log("Event received", req.body.type);
+  const { type, data } = req.body;
+
+  if (type === "CommentModerated") {
+    const { postId, id, status, content } = data;
+    const comments = commentsByPostId[postId];
+
+    const comment = comments.find((comment) => (comment.id === id));
+    comment.status = status;
+
+    await axios.post("http://localhost:4005/events", {
+      type: "CommentUpdated",
+      data: {
+        id,
+        postId,
+        status,
+        content,
+      },
+    });
+  }
+
   res.send({});
 });
 
--- backend/event-bus/index.js
+++ backend/event-bus/index.js
@@ -9,14 +9,18 @@ app.post('/events', (req, res) => {
     const event = req.body;
 
     axios.post('http://localhost:4000/events', event).catch(err => {
-        console.log(err)
+        console.log(err.message)
     });
     axios.post('http://localhost:4001/events', event).catch(err => {
-        console.log(err)
+        console.log(err.message)
     });
     axios.post('http://localhost:4002/events', event).catch(err => {
-        console.log(err)
+        console.log(err.message)
     });
+    axios.post('http://localhost:4003/events', event).catch(err => {
+        console.log(err.message)
+    });
+
     res.send({status: 'ok'});
 })
 
