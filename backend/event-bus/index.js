be662555e7f166a7916f4eab0390039a47ba283c
Renzo Navarro <rcnavarrop@gmail.com>
2022-12-01 20:03:39 -0500

    Implement event sync

--- backend/event-bus/index.js
+++ backend/event-bus/index.js
@@ -1,29 +1,37 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const axios = require('axios');
+const express = require("express");
+const bodyParser = require("body-parser");
+const axios = require("axios");
 
 const app = express();
 app.use(bodyParser.json());
 
-app.post('/events', (req, res) => {
-    const event = req.body;
+const events = [];
 
-    axios.post('http://localhost:4000/events', event).catch(err => {
-        console.log(err.message)
-    });
-    axios.post('http://localhost:4001/events', event).catch(err => {
-        console.log(err.message)
-    });
-    axios.post('http://localhost:4002/events', event).catch(err => {
-        console.log(err.message)
-    });
-    axios.post('http://localhost:4003/events', event).catch(err => {
-        console.log(err.message)
-    });
+app.post("/events", (req, res) => {
+  const event = req.body;
 
-    res.send({status: 'ok'});
-})
+  events.push(event);
+
+  axios.post("http://localhost:4000/events", event).catch((err) => {
+    console.log(err.message);
+  });
+  axios.post("http://localhost:4001/events", event).catch((err) => {
+    console.log(err.message);
+  });
+  axios.post("http://localhost:4002/events", event).catch((err) => {
+    console.log(err.message);
+  });
+  axios.post("http://localhost:4003/events", event).catch((err) => {
+    console.log(err.message);
+  });
+
+  res.send({ status: "ok" });
+});
+
+app.get("/events/", (req, res) => {
+  res.send(events);
+});
 
 app.listen(4005, () => {
-    console.log("Listening on port 4005 🔀")
-})
+  console.log("Listening on port 4005 🔀");
+});
--- backend/query/index.js
+++ backend/query/index.js
@@ -1,19 +1,14 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-
+const axios = require('axios');
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
 const posts = {};
 
-app.get("/posts", (req, res) => {
-  res.send(posts);
-});
-
-app.post("/events", (req, res) => {
-  const { type, data } = req.body;
+const handleEvent = (type, data) =>{
   if (type === "PostCreated") {
     const { id, title } = data;
     posts[id] = { id, title, comments: [] };
@@ -32,10 +27,27 @@ app.post("/events", (req, res) => {
     comment.status = status;
     comment.content = content;
   }
+}
+
+app.get("/posts", (req, res) => {
+  res.send(posts);
+});
+
+app.post("/events", (req, res) => {
+  const { type, data } = req.body;
+  
+  handleEvent(type, data);
 
   res.send({});
 });
 
-app.listen(4002, () => {
+app.listen(4002, async () => {
   console.log("Listening on 4002 🖥️");
+
+  const res = await axios.get("http://localhost:4005/events");
+
+  for (let event of res.data) {
+    console.log('Processing event:', event.type);
+    handleEvent(event.type, event.data);
+  }
 });
