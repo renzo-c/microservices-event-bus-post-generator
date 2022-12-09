ab52e93f9f0fbcc8bfe9ca9f638d2aeb5c2cf90f
Renzo Navarro <rcnavarrop@gmail.com>
2022-12-08 22:18:07 -0500

    Implement nodePort, clusterIP services, and deployments. Communicate posts and event-bus microservices

--- backend/posts/index.js
+++ backend/posts/index.js
@@ -23,7 +23,7 @@ app.post("/posts", async (req, res) => {
     title,
   };
 
-  await axios.post("http://localhost:4005/events", {
+  await axios.post("http://event-bus-srv:4005/events", {
     type: "PostCreated",
     data: { id, title },
   });
@@ -37,5 +37,6 @@ app.post("/events", (req, res) => {
 });
 
 app.listen(4000, () => {
+  console.log("v55")
   console.log("Listening on port 4000 💻");
 });
